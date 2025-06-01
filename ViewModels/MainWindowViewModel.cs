c559cc2aae73885d6c548cfd1760ca97835a9741
kevin <xiepeerless@gmail.com>
2025-06-01 16:30:30 -0500

    init project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace CumbyMinerScan.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private string _inputText;
+    private string _outputText;
+
+    public string InputText
+    {
+        get => _inputText;
+        set => this.RaiseAndSetIfChanged(ref _inputText, value);
+    }
+
+    public string OutputText
+    {
+        get => _outputText;
+        set => this.RaiseAndSetIfChanged(ref _outputText, value);
+    }
+
+    public ObservableCollection<TableItem> TableData { get; } = new ObservableCollection<TableItem>();
+
+    public ICommand SubmitCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        SubmitCommand = ReactiveCommand.Create(OnSubmit);
+    }
+
+    private void OnSubmit()
+    {
+        OutputText = $"你输入了: {InputText}";
+        TableData.Add(new TableItem { Column1 = InputText, Column2 = "已提交" });
+        InputText = string.Empty;
+    }
+    public class TableItem
+    {
+        public string Column1 { get; set; }
+        public string Column2 { get; set; }
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CumbyMinerScan.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
