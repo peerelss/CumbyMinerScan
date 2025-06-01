d8ce595da3dcde6e2cd7cad14832416ee251857e
kevin <xiepeerless@gmail.com>
2025-06-01 17:32:12 -0500

    add dialog

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Windows.Input;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using CumbyMinerScan.Models;
 using ReactiveUI;
 
 namespace CumbyMinerScan.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    public Interaction<Unit, string?> ShowFileDialog { get; }
     private string _inputText;
     private string _outputText;
+    public ReactiveCommand<Unit, Unit> OpenFileCommand { get; }
 
     public string InputText
     {
@@ -24,10 +32,26 @@ public class MainWindowViewModel : ViewModelBase
     public ObservableCollection<TableItem> TableData { get; } = new ObservableCollection<TableItem>();
 
     public ICommand SubmitCommand { get; }
+    public ICommand SelectCommand { get; }
 
     public MainWindowViewModel()
     {
         SubmitCommand = ReactiveCommand.Create(OnSubmit);
+        SelectCommand = ReactiveCommand.Create(OnSelect);
+        ShowFileDialog = new Interaction<Unit, string?>();
+        OpenFileCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            var filePath = await ShowFileDialog.Handle(Unit.Default);
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                OutputText = $"你选择的文件是：{filePath}";
+            }
+        });
+    }
+
+    private async void OnSelect()
+    {
+        OutputText = $"选择文件";
     }
 
     private void OnSubmit()
@@ -36,6 +60,7 @@ public class MainWindowViewModel : ViewModelBase
         TableData.Add(new TableItem { Column1 = InputText, Column2 = "已提交" });
         InputText = string.Empty;
     }
+
     public class TableItem
     {
         public string Column1 { get; set; }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,36 @@
+using System;
+using System.Reactive.Disposables;
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using CumbyMinerScan.ViewModels;
+using ReactiveUI;
 
 namespace CumbyMinerScan.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+        this.WhenActivated(disposables =>
+        {
+            ViewModel!.ShowFileDialog.RegisterHandler(async interaction =>
+            {
+                var dialog = new OpenFileDialog
+                {
+                    AllowMultiple = false,
+                    Filters =
+                    {
+                        new FileDialogFilter { Name = "CSV", Extensions = { "csv" } },
+                        new FileDialogFilter { Name = "Excel", Extensions = { "xls", "xlsx" } }
+                    }
+                };
+
+                var result = await dialog.ShowAsync(this);
+                interaction.SetOutput(result?.Length > 0 ? result[0] : null);
+            }).DisposeWith(disposables);
+        });
     }
+
+     
 }
